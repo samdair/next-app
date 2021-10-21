29f854e142bc8c4cdf4981c09de71b308c48cf9f
Samuel <samdair@yahoo.com>
2021-10-21 16:38:48 -0700

    removed the image

--- pages/index.js
+++ pages/index.js
@@ -10,14 +10,6 @@ export default function Home() {
       <Link href="/about">
         <a>About Page!</a>
       </Link>
-      <Image
-        src={profilePic}
-        alt="Picture of the author"
-        // width={500} automatically provided
-        // height={500} automatically provided
-        // blurDataURL="data:..." automatically provided
-        placeholder="blur" // Optional blur-up while loading
-      />
     </div>
   )
 }
