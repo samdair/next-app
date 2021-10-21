1eaf8b2005ad67aa08ca0f2faec75a07e23044fd
Samuel <samdair@yahoo.com>
2021-10-21 16:46:37 -0700

    build error

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
-import profilePic from '../public/sami.jpg'
 export default function Home() {
   return (
     <div>
